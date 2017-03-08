3b09fcd1b0da6f3e46f254b8ccd1fa48fb149b4e
Frank Fiebig <frank.fiebig@exozet.com>
2017-03-08 17:02:44 +0100

    add base plugin files

---
+++ README.md
@@ -0,0 +1,32 @@
+# faf-ipadmin
+
+Wordpress Plugin to restric IPs access to Admin Site.
+
+# Installation
+
+* Unzip and upload the plugin to the **/wp-content/plugins/** directory
+* Activate the plugin in WordPress
+* Got to plugins page and use the optimization.
+
+# Installation with composer
+
+* Add the repo to your composer.json
+
+```json
+
+"repositories": [
+    {
+        "type": "vcs",
+        "url": "https://github.com/fafiebig/faf-ipadmin.git"
+    }
+],
+
+```
+
+* require the package with composer
+
+```shell
+
+composer require fafiebig/faf-ipadmin 1.*
+
+```
---
+++ faf-ipadmin.php
@@ -0,0 +1,105 @@
+<?php
+/*
+Plugin Name: FAF IPAdmin
+Plugin URI: https://github.com/fafiebig/faf-ipadmin
+Description: WP Plugin to restrict IPs access to Admin Sites.
+Version: 1.0
+Author: F.A. Fiebig
+Author URI: http://fafworx.com
+License: GNU GENERAL PUBLIC LICENSE
+*/
+defined('ABSPATH') or die('No direct script access allowed!');
+
+/**
+ *
+ */
+if (is_admin()) {
+
+    // get remote ip
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+
+    // get ip whitelist
+    $restrict   = get_option('restrict', false);
+    $whitelist  = array_merge(['127.0.0.1'], explode(PHP_EOL, get_option('whitelist')));
+
+    // bail out if ip is not whitelisted
+    if ($restrict && !in_array($ip, $whitelist)) {
+        header('HTTP/1.0 403 Forbidden');
+        die('Access restricted!');
+    }
+
+    add_action('admin_menu', 'addIPAdminPage');
+    add_action('admin_init', 'registerIPAdmin');
+}
+
+/**
+ *
+ */
+function addIPAdminPage()
+{
+    add_options_page('IP Admin', 'IP Admin', 'manage_options', 'faf-ipadmin', 'addIPAdminPageForm');
+}
+
+/**
+ *
+ */
+function registerIPAdmin()
+{
+    register_setting('faf-ipadmin', 'restrict');
+    register_setting('faf-ipadmin', 'whitelist');
+}
+
+/**
+ *
+ */
+function addIPAdminPageForm()
+{
+    ?>
+    <div class="wrap exopress">
+        <h1>IP Settings</h1>
+
+        <form method="post" action="options.php">
+
+<?php
+    settings_fields('faf-ipadmin');
+    do_settings_sections('faf-ipadmin');
+?>
+
+            <table width="100%">
+                <tr>
+                    <td>
+                        <p>Hier können alle erlaubten IPs eingetragen werden.</p>
+                        <?php submit_button('Einstellungen speichern', 'primary alignright', 'settings'); ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+
+                        <dl>
+                            <dt>
+                                IP Restriktion
+                            </dt>
+                            <dd>
+                                <input type="checkbox" name="restrict" value="yes" <?php if (get_option('restrict') === 'yes') { ?>checked<?php } ?>>
+                                aktivieren
+                            </dd>
+                            <dt>IP Whitelist (eine IP pro Zeile)</dt>
+                            <dd>
+                                <textarea cols="80" rows="8" name="whitelist"><?php echo get_option('whitelist'); ?></textarea>
+                            </dd>
+                        </dl>
+
+                    </td>
+                </tr>
+            </table>
+        </form>
+
+    </div>
+    <?php
+}
